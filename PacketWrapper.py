da4c954e336b3188a23d5077383493cfe853214d
Not-MatanEilat <57912969+Not-MatanEilat@users.noreply.github.com>
2023-11-05 21:33:25 +0200

    Added toString to betterPacket

--- PacketWrapper.py
+++ PacketWrapper.py
@@ -11,6 +11,11 @@ class MACPacket:
     def __init__(self, packet: Packet):
         self.packet = packet
 
+    def __str__(self):
+        return f"MAC:" \
+                f"  Source MAC: {self.get_source_mac()}" \
+                f"  Destination MAC: {self.get_destination_mac()}\n"
+
     def get_packet(self) -> Packet:
         return self.packet
 
@@ -22,6 +27,12 @@ class MACPacket:
 
 
 class IPPacket(MACPacket):
+
+    def __str__(self):
+        return super().__str__() + f"IP:" \
+                            f"  Source IP: {self.get_source_ip()}" \
+                            f"  Destination IP: {self.get_destination_ip()}\n" \
+
     def get_source_ip(self) -> str:
         return self.packet[IP].src
 
@@ -30,6 +41,14 @@ class IPPacket(MACPacket):
 
 
 class TCPPacket(IPPacket):
+
+    def __str__(self):
+        return super().__str__() + f"TCP:" \
+                            f"  Source Port: {self.get_source_port()}" \
+                            f"  Destination Port: {self.get_destination_port}" \
+                            f"  Protocl: {self.get_protocol()}" \
+                            f"  Data: {self.get_data()}\n"
+
     def get_source_port(self) -> int:
         return self.packet[TCP].sport
 
@@ -41,3 +60,14 @@ class TCPPacket(IPPacket):
 
     def get_data(self) -> str:
         return self.packet[TCP].load
+
+
+def to_better_packet(packet):
+    if packet.haslayer(TCP):
+        return TCPPacket(packet)
+    elif packet.haslayer(IP):
+        return IPPacket(packet)
+    elif packet.haslayer(Ether):
+        return MACPacket(packet)
+    else:
+        return None
--- Scanner.py
+++ Scanner.py
@@ -1,10 +1,13 @@
+import PacketWrapper
 from scapy.sendrecv import sniff
 
 
 class Scanner:
 
     def handle_packet(self, packet):
-        print(packet.show())
+        better_packet = PacketWrapper.to_better_packet(packet)
+        if (better_packet is not None):
+            print(better_packet)
 
     def scan(self):
         sniff(prn=self.handle_packet)
