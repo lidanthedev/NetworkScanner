d45369b875af9b5a3039630f0b0e6b08f4c21941
Lidan <ls301205@gmail.com>
2023-11-13 11:50:02 +0200

    added class ARPPacket and ARPReplyPacket

--- PacketWrapper.py
+++ PacketWrapper.py
@@ -1,4 +1,4 @@
-from scapy.layers.l2 import Ether
+from scapy.layers.l2 import Ether, ARP
 from scapy.layers.inet import IP, TCP
 from scapy.packet import Packet
 import dataclasses
@@ -13,8 +13,8 @@ class MACPacket:
 
     def __str__(self):
         return f"MAC:" \
-                f"  Source MAC: {self.get_source_mac()}" \
-                f"  Destination MAC: {self.get_destination_mac()}\n"
+               f"  Source MAC: {self.get_source_mac()}" \
+               f"  Destination MAC: {self.get_destination_mac()}\n"
 
     def get_packet(self) -> Packet:
         return self.packet
@@ -30,9 +30,9 @@ class IPPacket(MACPacket):
 
     def __str__(self):
         return super().__str__() + f"IP: " \
-                            f"  Source IP: {self.get_source_ip()}" \
-                            f"  Destination IP: {self.get_destination_ip()}\n" \
-
+                                   f"  Source IP: {self.get_source_ip()}" \
+                                   f"  Destination IP: {self.get_destination_ip()}\n" \
+ \
     def get_source_ip(self) -> str:
         return self.packet[IP].src
 
@@ -44,8 +44,8 @@ class TCPPacket(IPPacket):
 
     def __str__(self):
         return super().__str__() + f"TCP:" \
-                            f"  Source Port: {self.get_source_port()}" \
-                            f"  Destination Port: {self.get_destination_port()}\n"
+                                   f"  Source Port: {self.get_source_port()}" \
+                                   f"  Destination Port: {self.get_destination_port()}\n"
 
     def get_source_port(self) -> int:
         return self.packet[TCP].sport
@@ -54,12 +54,41 @@ class TCPPacket(IPPacket):
         return self.packet[TCP].dport
 
 
+class ARPPacket(MACPacket):
+    def __str__(self):
+        return super().__str__() + f"ARP Request:" \
+                                   f"  Source IP: {self.get_source_ip()}" \
+                                   f"  Destination IP: {self.get_destination_ip()}\n"
+
+    def get_source_ip(self) -> str:
+        return self.packet[ARP].psrc
+
+    def get_destination_ip(self) -> str:
+        return self.packet[ARP].pdst
+
+
+class ARPReplyPacket(ARPPacket):
+    def __str__(self):
+        return super().__str__() + f"ARP Reply:" \
+                                   f"  Source IP: {self.get_source_ip()}" \
+                                   f"  Destination IP: {self.get_destination_ip()}\n"
+
+    def get_response_mac(self) -> str:
+        return self.packet[ARP].hwsrc
+
+
 def to_better_packet(packet):
     if packet.haslayer(TCP) and packet.haslayer(IP) and packet.haslayer(Ether):
         return TCPPacket(packet)
     elif packet.haslayer(IP) and packet.haslayer(Ether):
         return IPPacket(packet)
+    elif packet.haslayer(ARP) and packet.haslayer(Ether):
+        if packet[ARP].op == 1: # ARP Request
+            return ARPPacket(packet)
+        elif packet[ARP].op == 2: # ARP Reply
+            return ARPReplyPacket(packet)
     elif packet.haslayer(Ether):
         return MACPacket(packet)
     else:
         return None
+    return None
