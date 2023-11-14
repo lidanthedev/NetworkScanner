67b4858ac4d1e27a6fc574722c575f805d53f6b4
Not-MatanEilat <57912969+Not-MatanEilat@users.noreply.github.com>
2023-11-14 19:38:40 +0200

    Changed extract dns domain function to get_current_ssid

--- DHCPHandler.py
+++ DHCPHandler.py
@@ -1,4 +1,5 @@
 from AttackHandler import AttackHandler
+import subprocess
 from scapy.layers.dhcp import DHCP
 
 
@@ -11,12 +12,23 @@ class DHCPHandler(AttackHandler):
         if DHCP not in better_packet.packet:
             return
 
+        # if joined a new network
+        if self.is_packet_dhcp_ack(better_packet):
+
+            # save the mac address of the joined network if it's the first time joining it
+            network_name = self.get_current_ssid()
+            if network_name not in self.mac_table:
+                self.mac_table[network_name] = better_packet.get_source_mac()
+
+
+
+
+
         self.detect_attack(better_packet)
 
     def detect_attack(self, better_packet):
         if self.is_packet_dhcp_ack(better_packet):
-            dns_domain = self.extract_dns_domain(better_packet)
-            self.mac_table[better_packet.get_source_mac()] = dns_domain
+            self.mac_table[better_packet.get_source_mac()] = self.get_current_ssid()
 
     def protect_attack(self):
         pass
@@ -26,11 +38,10 @@ class DHCPHandler(AttackHandler):
     def is_packet_dhcp_ack(self, better_packet):
         return better_packet.packet[DHCP].options[0][1] == 5
 
-    def extract_dns_domain(self, better_packet):
-        dns_domain = None
-        if DHCP in better_packet.packet:
-            dhcp_options = better_packet.packet[DHCP].options
-            for option_code, option_data in dhcp_options:
-                if option_code == 15:  # Option 15 corresponds to DNS Domain Name
-                    dns_domain = option_data.decode('utf-8')
-        return dns_domain
+    def get_current_ssid(self):
+        # Run the 'iwgetid' command to get information about the current Wi-Fi connection
+        result = subprocess.check_output(['iwgetid', '--raw'], stderr=subprocess.STDOUT, text=True)
+
+        # Extract and return the SSID from the command output
+        ssid = result.strip()
+        return ssid
