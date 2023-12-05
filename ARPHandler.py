ee68da89f73f11047c44eb77c2eab9a55cd1c12d
Lidan <ls301205@gmail.com>
2023-12-05 21:56:57 +0200

    Made the ARPHandler automatically scan for mac addresses in your network DHCPHandler get_current_ssid defaults to Ethernet if it cannot find your wifi

--- ARPHandler.py
+++ ARPHandler.py
@@ -1,3 +1,8 @@
+import threading
+import logging
+
+from scapy.sendrecv import srp, sr1, sr
+
 from AttackHandler import AttackHandler
 from PacketWrapper import ARPPacket, ARPReplyPacket, MACPacket
 from scapy.layers.l2 import ARP, Ether
@@ -8,7 +13,11 @@ class ARPHandler(AttackHandler):
 
     def __init__(self):
         self.arp_table = {}
-        self.find_devices_on_network()
+
+        # run find_device_on_network in a thread
+
+        scanner_thread = threading.Thread(target=self.find_devices_on_network)
+        scanner_thread.start()
 
     def handle_packet(self, better_packet: MACPacket):
         if isinstance(better_packet, ARPPacket):
@@ -27,26 +36,13 @@ class ARPHandler(AttackHandler):
 
     def protect_attack(self):
         print("PROTECT ARP!!!!")
-    
-    def find_devices_on_network(self):
-        # using scapy send arp request to all ips on network
-        # if response is received then add to arp table
-        arp_request = ARP(pdst="192.168.1.0/24") / Ether(dst="ff:ff:ff:ff:ff:ff")
-        my_ip = arp_request[ARP].psrc
-        self.find_device_on_network()
-        
-    
-    def find_device_on_network(self, my_ip, last_digit: int):
-        
-        ip_split = my_ip.split(".")
-        ip_split[3] = str(last_digit)
-        broadcast_ip = ".".join(ip_split)
-        
-        arp_request[ARP].pdst = broadcast_ip
-        ARP_request_broadcast = broadcast/ARP_request
-        answered_list = srp(ARP_request_broadcast, timeout=1, verbose=False)[0]
 
-        for element in answered_list:
-            self.arp_table[element[1].psrc] = element[1].hwsrc
-            print("IP: " + element[1].psrc + " MAC: " + element[1].hwsrc)
-        
+    def find_devices_on_network(self):
+        scapy_logger = logging.getLogger("scapy.runtime")
+        old_level = scapy_logger.level
+        scapy_logger.setLevel(logging.ERROR)
+        arp_request = ARP() / Ether(dst="ff:ff:ff:ff:ff:ff")
+        arp_request.pdst = arp_request.psrc + "/24"
+        sr(arp_request, timeout=1, verbose=False)
+        scapy_logger.setLevel(old_level)
+        print("ARP SCAN COMPLETE")
