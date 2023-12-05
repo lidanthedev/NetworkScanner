f5f53afd243ab8d4dac18bba1c9355093406b045
LidanTheDev <ls301205@gmail.com>
2023-12-05 18:55:56 +0200

    starting to try to improve the ArpHandler

--- ARPHandler.py
+++ ARPHandler.py
@@ -1,5 +1,6 @@
 from AttackHandler import AttackHandler
 from PacketWrapper import ARPPacket, ARPReplyPacket, MACPacket
+from scapy.layers.l2 import ARP, Ether
 
 
 class ARPHandler(AttackHandler):
@@ -7,6 +8,7 @@ class ARPHandler(AttackHandler):
 
     def __init__(self):
         self.arp_table = {}
+        self.find_devices_on_network()
 
     def handle_packet(self, better_packet: MACPacket):
         if isinstance(better_packet, ARPPacket):
@@ -25,3 +27,26 @@ class ARPHandler(AttackHandler):
 
     def protect_attack(self):
         print("PROTECT ARP!!!!")
+    
+    def find_devices_on_network(self):
+        # using scapy send arp request to all ips on network
+        # if response is received then add to arp table
+        arp_request = ARP(pdst="192.168.1.0/24") / Ether(dst="ff:ff:ff:ff:ff:ff")
+        my_ip = arp_request[ARP].psrc
+        self.find_device_on_network()
+        
+    
+    def find_device_on_network(self, my_ip, last_digit: int):
+        
+        ip_split = my_ip.split(".")
+        ip_split[3] = str(last_digit)
+        broadcast_ip = ".".join(ip_split)
+        
+        arp_request[ARP].pdst = broadcast_ip
+        ARP_request_broadcast = broadcast/ARP_request
+        answered_list = srp(ARP_request_broadcast, timeout=1, verbose=False)[0]
+
+        for element in answered_list:
+            self.arp_table[element[1].psrc] = element[1].hwsrc
+            print("IP: " + element[1].psrc + " MAC: " + element[1].hwsrc)
+        
