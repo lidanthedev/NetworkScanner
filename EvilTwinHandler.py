59d429c4b9e4edd5ef49b5823b1c68b4cbf1f5ae
Not-MatanEilat <57912969+Not-MatanEilat@users.noreply.github.com>
2023-12-09 20:22:35 +0200

    Fixed a problen with getting current networks, now works

--- EvilTwinHandler.py
+++ EvilTwinHandler.py
@@ -33,15 +33,15 @@ class EvilTwinHandler(AttackHandler):
 
     def get_wifi_networks(self):
         try:
-            result = subprocess.check_output(['iwlist', 'scan'], stderr=subprocess.STDOUT, text=True)
+            result = subprocess.run(['iwlist', 'scan'], capture_output=True, text=True)
 
             if result.returncode == 0:
                 networks = [line.split(':')[1].strip() for line in result.stdout.split('\n') if 'ESSID' in line]
                 return networks
             else:
-                print(f"Error: {result.stderr}")
+                print(f"Error in EvilTwin: {result.stderr} (Are you in Windows?)")
                 return None
 
         except Exception as e:
-            print(f"An error occurred: {e}")
+            print(f"An error occurred with EvilTwin: {e} (Are you in Windows?)")
             return None
