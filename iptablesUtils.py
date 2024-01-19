5ab47917f4a130588c8c27b436dfd83c21bae391
LidanTheDev <ls301205@gmail.com>
2024-01-20 01:49:57 +0200

    made iptablesUtils.py only affect incoming packets

--- iptablesUtils.py
+++ iptablesUtils.py
@@ -7,17 +7,7 @@ def add_ip_table(id_table):
     :param id_table: The ID of the table.
     :return None
     """
-    # Create a new rule
-    rule = iptc.Rule()
-
-    # Create a target for the rule
-    target = iptc.Target(rule, "NFQUEUE")
-
-    # Set the queue number to the target
-    target.set_parameter("queue-num", str(id_table))
-
-    # Set the rule's target
-    rule.target = target
+    rule = make_rule(id_table)
 
     # Get the "INPUT" chain
     chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
@@ -33,16 +23,7 @@ def remove_ip_table(id_table):
     :return: None
     """
     # Create a new rule
-    rule = iptc.Rule()
-
-    # Create a target for the rule
-    target = iptc.Target(rule, "NFQUEUE")
-
-    # Set the queue number to the target
-    target.set_parameter("queue-num", str(id_table))
-
-    # Set the rule's target
-    rule.target = target
+    rule = make_rule(id_table)
 
     # Get the "INPUT" chain
     chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
@@ -50,4 +31,22 @@ def remove_ip_table(id_table):
     # Delete the rule from the chain
     chain.delete_rule(rule)
 
-# manual disable: sudo iptables -D INPUT -j NFQUEUE --queue-num 0
+
+def make_rule(id_table):
+    """
+    Make a rule for the INPUT chain.
+    :param id_table: The ID of the table.
+    :return: The rule.
+    """
+    rule = iptc.Rule()
+    rule.src = "!127.0.0.1"
+    # Create a target for the rule
+    target = iptc.Target(rule, "NFQUEUE")
+    # Set the queue number to the target
+    target.set_parameter("queue-num", str(id_table))
+    # Set the rule's target
+    rule.target = target
+    return rule
+
+# manual disable: sudo iptables -L INPUT --line-numbers
+# sudo iptables -D INPUT [NUM]
