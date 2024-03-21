ddabfcb5d43445168186c917e1b431e93847a159
LidanTheDev <ls301205@gmail.com>
2024-03-21 16:21:24 +0200

    added basic PieChart

--- NetScanWeb/src/App.tsx
+++ NetScanWeb/src/App.tsx
@@ -4,6 +4,15 @@ import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import Scanner from "./Scanner.tsx";
 import AttacksAccordions from "./AttacksAccordions.tsx";
 import Logs from "./Logs.tsx";
+import { PieChart } from "./PieChart.tsx";
+
+const data = [
+  { name: "Mark", value: 90 },
+  { name: "Robert", value: 12 },
+  { name: "Emily", value: 34 },
+  { name: "Marion", value: 53 },
+  { name: "Nicolas", value: 98 },
+]
 
 function App() {
   const [darkMode, setDarkMode] = useState('dark')
@@ -14,12 +23,13 @@ function App() {
     },
   });
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline/>
-      <Scanner/>
-      <AttacksAccordions/>
-      <Logs/>
-    </ThemeProvider>
+    // <ThemeProvider theme={darkTheme}>
+    //   <CssBaseline/>
+    //   <Scanner/>
+    //   <AttacksAccordions/>
+    //   <Logs/>
+    // </ThemeProvider>
+    <PieChart data={data} width={400} height={400} />
   )
 }
 
